6ff1e63bb9add10fb267d2d0892650e3bba5f370
Martin Weber <mweber@hh-berlin.de>
2018-06-06 10:48:55 +0200

    implements more test cases

--- public/js/client.js
+++ public/js/client.js
@@ -1,58 +1,115 @@
-
 window.clientId = '778';
 window.clientURL = 'http://localhost:3000';
-document.addEventListener("DOMContentLoaded", function() {
+
+const ownAccessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvd25lcjo0ODZiODZjZS0wODIzLTQxYjAtYmFjNy1jNDliYTdkN2Y2ZTgiLCJpc3MiOiJ1cm46Z2hjIiwiZXhwIjoxNTI4Mjc4Mzk4LCJuYmYiOjE1MjgyNzQ2NzgsImlhdCI6MTUyODI3NDczOCwianRpIjoiNjVhZjk4NTUtMTI0OS00OWY5LTljMzktYzZkNzlhNWMwYWIzIiwiZ2hjOmFpZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsImdoYzpjaWQiOiIyIiwiZ2hjOnVpZCI6IjQ4NmI4NmNlLTA4MjMtNDFiMC1iYWM3LWM0OWJhN2Q3ZjZlOCIsImdoYzpzY29wZSI6ImV4YyBwZXJtOnIgcGVybTp3IHJlYzpyIHJlYzp3IGF0dGFjaG1lbnQ6ciBhdHRhY2htZW50OncgdXNlcjpyIHVzZXI6dyB1c2VyOnEifQ.Q2Mr1zuIlLCoQErsXUjsPWzhPEyS5UZBzb17F-HU15Gr1gpV8EePHhZEheDRyBn8aSo_87yoe0hK2tnMBuzM1eha5ExrGKzY8MQYjex7osTOsL2pIMIs7KDpphJiJlmws51L76vy1v2QgHhUfmuqzAQQEWoPoZG3u4mtCOSzjIZnr_6XSVnQXQVt3pGrD9JkfP6QYQV036euEEfqa0TTStupJ2wpdVE8NE1FhtQaf5ICu20MHw7fsJRaBGFCRH4BADZTyWlC3oytAGej81SKVoMbBPRDQPBkBvrn_VWQLghfmH5rUnZ-MlRCadF41h3Y6tqarA7HiK3mcXtaDx-jvyObDXT9FUbtfJ-9Zo8Q7rgPF5xVoQeLC5E2fjtvsDbEppcZj7FWa0l0e3sK7bZDVzkDf6XoSsiLdWa-i7leNuv9xjWWIBnMQnCqwCv65jD05WTIzYxixM602AsZt9WNaoknOLeKMEuKADcNlm7h7Kzt1kfyd9f3o87pW809hn2uaNSzI6cxLpmpDEZc4iH_oq2HothxXBXwTeIIjyv_zbNEBhlLPoglHZTWkPkOSP9tdIJYSN1cCmLLAUo9KOrooy1xvOs5eJgSveL9FTx1lJ-elGcX8B1ZH1u0vQDxQro2WKngJ8q4zDyntkRPqvZLFHFlG8ophcEQMRwG-Q6oSPg";
+const ownUserId = "486b86ce-0823-41b0-bac7-c49ba7d7f6e8";
+
+const dummyFile = createDummyFile();
+
+document.addEventListener("DOMContentLoaded", function () {
     const userId = GC.SDK.getCurrentUserId();
     const appId = GC.SDK.getCurrentAppId();
 
     console.log(userId);
     console.log(appId);
 
-    /* LOGIN over HPI Auth */
-
     /* SETUP Authentification */
-    // TODO Implement OAuth Client, kein Plan wie die API da funktionert....
-    // fehlenden Routes , /login, /gctoken, /gckeys
-    /*GC.AUTH.config({
-        clientId: window.clientId,
-        clientURL: window.clientURL,
-    });
-    GC.AUTH.loggedIn.then((isLoggedIn) => {
-        if(!isLoggedIn){
-            GC.AUTH.login();
-        }
-    });*/
-
-
-    /*  */
     showKeys(localStorage.getItem('publicKey'), localStorage.getItem('privateKey'));
 
-    document.getElementById("btnSetupSdk").addEventListener("click", function () {
-        setupSdk();
-    });
+    document.getElementById("btnSetupSdk").addEventListener("click", setupSdk);
+    document.getElementById("btnLogin").addEventListener("click", loginOverAuthSdk);
+    document.getElementById("btnLoadData").addEventListener("click", loadData);
+    document.getElementById("btnUploadData").addEventListener("click", uploadData);
 });
 
 function setupSdk() {
     GC.SDK.createCAP().then((keys) => {
         localStorage.setItem('publicKey', keys.publicKey);
-        localStorage.setItem('privateKey',keys.privateKey);
+        localStorage.setItem('privateKey', keys.privateKey);
         showKeys(keys.publicKey, keys.privateKey);
 
+        // GET ERRRO cant get user ID
         const promise = GC.SDK.setup(
             window.clientId,
             keys.privateKey,
-            function(){
-                return new Promise(function(resolve){
-                    //imporiert
-                    resolve("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvd25lcjo0ODZiODZjZS0wODIzLTQxYjAtYmFjNy1jNDliYTdkN2Y2ZTgiLCJpc3MiOiJ1cm46Z2hjIiwiZXhwIjoxNTI4MjEzNzkyLCJuYmYiOjE1MjgyMTAwNzIsImlhdCI6MTUyODIxMDEzMiwianRpIjoiNDYwMmRjMGYtYWQ2Yi00NmJmLTkxOTUtODY5NWQ5YTgwMWViIiwiZ2hjOmFpZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsImdoYzpjaWQiOiIyIiwiZ2hjOnVpZCI6IjQ4NmI4NmNlLTA4MjMtNDFiMC1iYWM3LWM0OWJhN2Q3ZjZlOCIsImdoYzpzY29wZSI6ImV4YyBwZXJtOnIgcGVybTp3IHJlYzpyIHJlYzp3IGF0dGFjaG1lbnQ6ciBhdHRhY2htZW50OncgdXNlcjpyIHVzZXI6dyB1c2VyOnEifQ.Qhf4J5Xxy0OaqBN-bwcj8-HgKJ91gQh7-hacS94craH0FR6prx2HTZzVt6KVQtAu9kOCCUCZcIXDpAPfOkrsKwAtvxfBCLN54SoJNiXwrUG1bbAgm445HkHJZmPrdlfXnzw12VppfA3xB7-zzpdU3ULqL4nRRF5AQLBE4eRdztgzixNMUxewzUyNUwUy1_XMm5DnrbFD6Jcf2TrwOqlE55PwQHrBBeLbmq6U--pZfvGKHImtWURCfSmxq3SuApZHbGAMrNrt6etz8nnA1Pg41j_7110V2UvYVhJlg31wKGupdP8PAHo9aZzwRE7mibthduip0U_iZvO8WaFTuIO338hXQy00hAO_fOML3V_ecu6zS1IQrrYtpgzYOBAUcazbhKI_layYUG0ioPySHyDGAZ7Qu9ILm8wpbGepOrd0dk3qcB1MbVKI0-qE_TkpJWhfqi3-KaYporA2miOC35SO6guiNmSlKAH7vbssAjjjo88MxKi1sXHOb5xIHB2gB7SHnfAFhlcY06rH0mRSPGdMvaLjrJU_t4BQNWScChDEAYJhRUPeTC2sCR28_kJ1FLWqIOwtpLMPiokSCFktCMcsqJGBRaywcADrkP9jDfA7UdrOCIWzSarUfhPycbKbHbEUT-7wxVkbmWYKwPgfIS0_Btko9f7viQ4NGV_4XNgy3X4");
+            function () {
+                return new Promise(function (resolve) {
+                    //imporiert --> wie kann ich den OAuth realisieren?
+                    resolve(ownAccessToken);
                 })
             }
         );
-        console.log(promise);
     })
 }
 
-function showKeys(publicKey, privateKey){
+function loginOverAuthSdk() {
+    /* LOGIN over HPI Auth */
+
+    GC.AUTH.config({
+        clientId: window.clientId,
+        clientURL: window.clientURL,
+    });
+    // TODO Implement OAuth Client, kein Plan wie die API da funktionert.... habe ich etwas falsch konfiguriert?
+    // fehlenden Routes , /login, /gctoken, /gckeys
+    GC.AUTH.loggedIn.then((isLoggedIn) => {
+        if (!isLoggedIn) {
+            GC.AUTH.login();
+        }
+    });
+}
+
+function loadData() {
+    console.log("LOAD DATA");
+    console.log("==========");
+    GC.SDK.getDocuments(ownUserId, {}).then((data) => {
+        console.log(data);
+    })
+}
+
+function uploadData() {
+    console.log("UPLAOD DATA");
+    console.log("==========");
+
+    console.log(dummyFile instanceof File);
+
+    const files = [dummyFile];
+
+    //TODO ist die Struktur so korrek, in der DOUK gibt es unterschiedliche Angaben
+    const document = new GC.SDK.models.HCDocument({
+        files,
+        title: "SA Test",
+        author: new GC.SDK.models.HCAuthor({firstName: "Max", lastName: "Mustermann"}),
+        type: "application/json",
+        creationDate: new Date(),
+        annotations: ["Situationsanalyse"] //fuer spätere Filterung
+    });
+
+    console.log("Document: ", document);
+    GC.SDK.uploadDocument(ownUserId, document)
+        .then((response) => console.log(response));
+
+
+}
+
+function createDummyFile() {
+    const data = {SA_Text: "Hello World", PHQ_9: 5};
+    const json = JSON.stringify(data);
+    const blob = new Blob([json], {type: "application/json"});
+    const file = new File([blob], "SA.json", {type: "text/json;charset=utf-8", lastModified: Date.now()});
+    const url = URL.createObjectURL(blob);
+
+    let a = document.createElement('a');
+    a.download = "SA.json";
+    a.href = url;
+    a.textContent = "Download SA.json";
+
+    document.getElementById("dummyFile").appendChild(a);
+
+    return file;
+}
+
+
+function showKeys(publicKey, privateKey) {
     document.getElementById("publicKey").textContent = publicKey;
     document.getElementById("privateKey").textContent = privateKey;
 }
